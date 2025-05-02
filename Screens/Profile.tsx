408063aa29a7e8b046c71e6da3b1a5b1cd6a9069
dongyeon <ldy1951123@gmail.com>
2025-05-03 00:11:37 +0900

    마이페이지 수정중

--- Screens/Profile.tsx
+++ Screens/Profile.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 import Header from '../navigation/Header';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Ionicons } from '@expo/vector-icons';
 
 const Profile :React.FC = () => {
   const handleNotificationPress = () => {
@@ -23,8 +24,44 @@ const Profile :React.FC = () => {
       <Header title="마이페이지" onNotificationPress={handleNotificationPress} />
 
       <ScrollView contentContainerStyle={styles.content}>
-        {/* 메인 콘텐츠 */}
-        <Text style={styles.text}>프로필 화면</Text>
+        <View style={styles.comment}>
+            <Ionicons name="rocket-outline" size={12} color="#3D3D3D" style={styles.icon} />
+            <Text style={styles.commentText}>여기는 개발자 코멘트를 적는 곳.</Text>
+        </View>
+        <View style={styles.introduce}>
+            <Image source={require('../img/Profile.jpg')} style={styles.image} />
+            <View style={styles.infoBox}>
+                <Text style={styles.label}>이름: <Text style={styles.value}>이동연</Text></Text>
+                <Text style={styles.label}>닉네임: <Text style={styles.value}>강아지똥</Text></Text>
+                <Text style={styles.label}>생년월일: <Text style={styles.value}>2001.01.23</Text></Text>
+            </View>
+        </View>
+        <View style={styles.myInfo}>
+            <View style={styles.infoRow}>
+                <Text style={styles.infoLabel}>학과</Text>
+                <Text style={styles.infoValue}>컴퓨터공학과</Text>
+            </View>
+            <View style={styles.underline} />
+            <View style={styles.infoRow}>
+                <Text style={styles.infoLabel}>학번</Text>
+                <Text style={styles.infoValue}>20학번</Text>
+            </View>
+            <View style={styles.underline} />
+            <View style={styles.infoRow}>
+                <Text style={styles.infoLabel}>나이</Text>
+                <Text style={styles.infoValue}>25</Text>
+            </View>
+            <View style={styles.underline} />
+            <View style={styles.infoRow}>
+                <Text style={styles.infoLabel}>키</Text>
+                <Text style={styles.infoValue}>175cm</Text>
+            </View>
+            <View style={styles.underline} />
+            <View style={styles.infoRow}>
+                <Text style={styles.infoLabel}>전화번호</Text>
+                <Text style={styles.infoValue}>010-3200-1951</Text>
+            </View>
+        </View>
       </ScrollView>
     </View>
 
@@ -42,8 +79,86 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: 20,
+  comment: {
+    width: '90%',
+    height: 30,
+    backgroundColor: '#D1D0D0',
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  commentText: {
+    fontSize: 11,
+    color: '#3D3D3D',
+  },
+  icon: {
+    marginRight: 5,
+  },
+  introduce: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 12,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    margin: 20,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginRight: 40,
+  },
+  infoBox: {
+    flex: 1,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginBottom: 6,
+    color: '#333',
+  },
+  value: {
+    fontWeight: '400',
+    color: '#555',
+  },
+  myInfo: {
+    width: '90%',
+    backgroundColor: '#f9f9f9',
+    borderRadius: 12,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    padding: 30,
+  },
+  
+  infoRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 15,
+  },
+  
+  infoLabel: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#333',
+  },
+  
+  infoValue: {
+    fontSize: 16,
+    color: '#555',
+  },
+  underline: {
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginVertical: 1,
+    marginBottom: 20,
   },
 });
 
