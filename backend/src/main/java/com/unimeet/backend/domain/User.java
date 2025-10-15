707091bf039fb0b80e1928c7ca0c5c408ac47c6f
simjjjj <thdrnfldkwk@naver.com>
2025-10-15 20:50:03 +0900

    User 도메인 Lombok 어노테이션 수정

--- backend/src/main/java/com/unimeet/backend/domain/User.java
+++ backend/src/main/java/com/unimeet/backend/domain/User.java
@@ -1,7 +1,9 @@
 package com.unimeet.backend.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
@@ -14,6 +16,8 @@ import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "users")
 public class User implements UserDetails {
     @Id
